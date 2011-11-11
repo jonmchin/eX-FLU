35d9ef742bdd582421eaf6d345788fdd0bd5d97c
Jon <jonmchin@umich.edu>
2011-11-11 15:43:20 -0500

    Source

--- src/com/eXFLU/SurveyNotification/Survey.java
+++ src/com/eXFLU/SurveyNotification/Survey.java
@@ -28,38 +28,15 @@ public class Survey extends Activity
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			//goBacktoMain(); //for some reason it doesn't go back to the main thread
-			sendNotification(); //works, although user has to click on notification
+			goBacktoMain(); //works now
         	//need to go back to the main thread
 	    }
 	   
 	   private void goBacktoMain()
 	   {
 		   Intent mainIntent = new Intent(this, SurveyNotification.class);
+		   mainIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
 		   startActivity(mainIntent);
+		  
 	   }
-	   private void sendNotification()
-	    {
-	    	String ns = Context.NOTIFICATION_SERVICE;
-	    	NotificationManager mNotificationManager = (NotificationManager) getSystemService(ns);
-	    
-	    	int icon = R.drawable.icon;
-	    	CharSequence tickerText = "Go Back";
-	    	long time = System.currentTimeMillis();
-
-	    	Notification notification = new Notification(icon, tickerText, time);
-	    	notification.flags |= Notification.FLAG_AUTO_CANCEL;
-	    
-	    	Context context = getApplicationContext();
-	    	CharSequence contentTitle = "Go Back";
-	    	CharSequence contentText = "Go Back to main thread";
-	    	
-	    	Intent notificationIntent = new Intent(this, SurveyNotification.class);
-	    	PendingIntent contentIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0);
-	    	notification.setLatestEventInfo(context, contentTitle, contentText, contentIntent);
-
-	    	final int Notification_ID = 1; //unique id for this specific notification
-	    	mNotificationManager.notify(Notification_ID, notification);	
-	    }
-
 }
